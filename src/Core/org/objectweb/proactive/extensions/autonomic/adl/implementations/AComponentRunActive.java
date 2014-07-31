dc5c579bfbed0aee5e1e4febd4732791b30d6002
Matias Ibañez <mibanez@fastmail.fm>
2014-07-31 12:33:20 -0400

    fix last commit

---
+++ src/Core/org/objectweb/proactive/extensions/autonomic/adl/implementations/AComponentRunActive.java
@@ -0,0 +1,9 @@
+package org.objectweb.proactive.extensions.autonomic.adl.implementations;
+
+import java.io.Serializable;
+
+import org.objectweb.proactive.core.component.body.ComponentRunActive;
+
+public interface AComponentRunActive extends ComponentRunActive, Serializable {
+
+}
