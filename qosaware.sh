3763ba7155fa8a30957bf038109bce650563c959
Cristian Ruz (hercules) <cruz@ing.puc.cl>
2015-06-26 02:29:23 -0300

    Modified starting script with SPRINGOO_HOME property

--- qosaware.sh
+++ qosaware.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-export JCP="/user/cruz/git/mape-component-controllers/build/*"
-JCP="$JCP:/user/cruz/git/mape-component-controllers/lib/*"
-JCP="$JCP:/user/cruz/git/gcmscript/dist/lib/*"
-JCP="$JCP:/user/cruz/git/programming-multiactivities/dist/lib/*"
+SPRINGOO_HOME="/user/cruz/git/mape-component-controllers"
+
+export JCP="$SPRINGOO_HOME/build/*"
+JCP="$JCP:$SPRINGOO_HOME/lib/*"
+JCP="$JCP:$SPRINGOO_HOME/lib/gcmscript-lib/*"
+JCP="$JCP:$SPRINGOO_HOME/lib/pa-ma-lib/*"
 
 export VM="-Dgcm.provider=org.objectweb.proactive.core.component.Fractive"
 VM="$VM -Dfractal.provider=org.objectweb.proactive.core.component.Fractive"
 VM="$VM -Djava.security.manager"
 VM="$VM -Djava.security.policy=/user/cruz/git/programming-multiactivities/dist/proactive.java.policy"
 VM="$VM -Dlog4j.configuration=file:/user/cruz/git/programming-multiactivities/dist/proactive-log4j"
-VM="$VM -Duser.home=/user/cruz/git"
-VM="$VM -Dproactive.home=/user/cruz/git/programming-multiactivities"
+#VM="$VM -Duser.home=/user/cruz/git"
+#VM="$VM -Dproactive.home=/user/cruz/git/programming-multiactivities"
 VM="$VM -Djline.terminal=jline.UnsupportedTerminal"
 
 export TEST=cl.niclabs.autonomic.examples.qosaware.Test
